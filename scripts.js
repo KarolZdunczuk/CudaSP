49159fc2901e1e2c13fc0727731d0fa19dd91819
Karol <berimbau28@gmail.com>
2019-04-30 20:26:55 +0200

    Adding skills section and jQuerry plugin

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-const hamburger = document.querySelector('.hamburger');
-hamburger.addEventListener("click", function(){
-    document.querySelector('.page-nav').classList.toggle('nav-opened');
-})
+$(".progress-bar").loading();
+$('input').on('click', function () {
+     $(".progress-bar").loading();
+});
