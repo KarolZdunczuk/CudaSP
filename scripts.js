e9ed0ac899107447ec3a03123a540c43170852ff
Karol <berimbau28@gmail.com>
2019-05-14 20:57:10 +0200

    added hamburger menu

--- scripts.js
+++ scripts.js
@@ -2,3 +2,8 @@ $(".progress-bar").loading();
 $('input').on('click', function () {
      $(".progress-bar").loading();
 });
+
+const hamburger = document.querySelector('.hamburger');
+hamburger.addEventListener("click", function(){
+    document.querySelector('.page-nav').classList.toggle('nav-opened');
+})
